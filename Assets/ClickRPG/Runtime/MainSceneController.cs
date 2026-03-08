c2cf2847a88439add66b59d5c1b34d3cf37850c7
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-08 23:39:47 +0900

    MainSceneControllerにUIViewの配列を追加し、Startメソッドでアクティブ化する処理を実装しました。

--- Assets/ClickRPG/Runtime/MainSceneController.cs
+++ Assets/ClickRPG/Runtime/MainSceneController.cs
@@ -4,5 +4,16 @@ namespace ClickRPG
 {
     public sealed class MainSceneController : MonoBehaviour
     {
+        [SerializeField]
+        private UIView[] uiViews = null!;
+
+        private void Start()
+        {
+            var context = new MainSceneContext();
+            foreach (var uiView in uiViews)
+            {
+                uiView.Activate(context);
+            }
+        }
     }
 }
