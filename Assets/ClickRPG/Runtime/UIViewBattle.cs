ccb3ea8dd6bbe9f94b220706e50c92ce1adc6bea
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-08 18:37:06 +0900

    UIViewBattleクラスを追加し、MainSceneContextを引数に取るActivateメソッドを定義しました。

---
+++ Assets/ClickRPG/Runtime/UIViewBattle.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace ClickRPG
+{
+    public sealed class UIViewBattle : UIView
+    {
+        public override void Activate(MainSceneContext context)
+        {
+        }
+    }
+}
