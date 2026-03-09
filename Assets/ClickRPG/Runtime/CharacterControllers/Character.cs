bf132889a9ce5dbd92c012d60ca1fb79074886e8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-09 23:34:47 +0900

    CharacterクラスにICharacterBrainフィールドと初期化ロジックを追加しました。

--- Assets/ClickRPG/Runtime/CharacterControllers/Character.cs
+++ Assets/ClickRPG/Runtime/CharacterControllers/Character.cs
@@ -1,8 +1,18 @@
+using ClickRPG.CharacterControllers.Brains;
 using UnityEngine;
 
 namespace ClickRPG.CharacterControllers
 {
     public sealed class Character : MonoBehaviour
     {
+        [SerializeReference, SubclassSelector]
+        private ICharacterBrain brain = null!;
+
+        private readonly CharacterBrainController brainController = new();
+
+        void Awake()
+        {
+            brainController.Setup(this, brain);
+        }
     }
 }
