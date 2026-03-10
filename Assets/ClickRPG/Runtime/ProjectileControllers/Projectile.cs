11941174b687dd5dd3b7ff66001d963ecae9ac0f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-10 16:04:46 +0900

    ProjectileクラスにRigidbody2Dとアクションリストを追加し、オブジェクトプールの実装を強化しました。

--- Assets/ClickRPG/Runtime/ProjectileControllers/Projectile.cs
+++ Assets/ClickRPG/Runtime/ProjectileControllers/Projectile.cs
@@ -1,8 +1,47 @@
+using System.Collections.Generic;
+using ClickRPG.CharacterControllers;
+using ClickRPG.ProjectileControllers.Actions;
+using R3;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace ClickRPG.ProjectileControllers
 {
     public sealed class Projectile : MonoBehaviour
     {
+        [field: SerializeField]
+        public Rigidbody2D Rigidbody2D { get; private set; } = null!;
+
+        [SerializeReference, SubclassSelector]
+        private List<IProjectileAction> actions = null!;
+
+        private ObjectPool<Projectile> pool = null!;
+
+        public CompositeDisposable Scope { get; } = new();
+
+        public Projectile Spawn(Character owner)
+        {
+            pool ??= new ObjectPool<Projectile>(
+                createFunc: () => Instantiate(this),
+                actionOnRelease: projectile =>
+                {
+                    projectile.gameObject.SetActive(false);
+                    projectile.Scope.Clear();
+                },
+                actionOnGet: projectile => projectile.gameObject.SetActive(true)
+                );
+            var instance = pool.Get();
+            instance.pool = pool;
+            foreach (var action in instance.actions)
+            {
+                action.Invoke(instance);
+            }
+            return instance;
+        }
+
+        public void Release()
+        {
+            pool.Release(this);
+        }
     }
 }
