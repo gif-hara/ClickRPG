f4eb3bd182a9935511e23c0bc41f3da0e46c5c0b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-10 22:12:54 +0900

    Characterクラスに能力配列を追加し、Awakeメソッドで初期化処理を実装しました。

--- Assets/ClickRPG/Runtime/CharacterControllers/Character.cs
+++ Assets/ClickRPG/Runtime/CharacterControllers/Character.cs
@@ -1,4 +1,6 @@
+using ClickRPG.CharacterControllers.Abilities;
 using ClickRPG.CharacterControllers.Brains;
+using SoulLike;
 using UnityEngine;
 
 namespace ClickRPG.CharacterControllers
@@ -8,10 +10,21 @@ namespace ClickRPG.CharacterControllers
         [SerializeReference, SubclassSelector]
         private ICharacterBrain brain = null!;
 
+        [SerializeReference, SubclassSelector]
+        private ICharacterAbility[] abilities = null!;
+
+        private readonly MessageBroker broker = new();
+
+        public IMessageBroker Broker => broker;
+
         private readonly CharacterBrainController brainController = new();
 
         void Awake()
         {
+            foreach (var ability in abilities)
+            {
+                ability.Initialize(this);
+            }
             brainController.Setup(this, brain);
         }
     }
