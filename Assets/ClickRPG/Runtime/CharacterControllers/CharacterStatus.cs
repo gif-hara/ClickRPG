f3e6216040083fd174a933fc98beead8478eeb21
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-10 22:13:42 +0900

    CharacterStatusクラスのプロパティにMin属性を追加し、CooldownLevelの初期値を1から0に変更しました。

--- Assets/ClickRPG/Runtime/CharacterControllers/CharacterStatus.cs
+++ Assets/ClickRPG/Runtime/CharacterControllers/CharacterStatus.cs
@@ -6,13 +6,13 @@ namespace ClickRPG.CharacterControllers
     [Serializable]
     public sealed class CharacterStatus
     {
-        [field: SerializeField]
+        [field: SerializeField, Min(0)]
         public int HitPoint { get; private set; } = 100;
 
-        [field: SerializeField]
+        [field: SerializeField, Min(0)]
         public int Strength { get; private set; } = 10;
 
-        [field: SerializeField]
-        public int CooldownLevel { get; private set; } = 1;
+        [field: SerializeField, Min(0)]
+        public int CooldownLevel { get; private set; } = 0;
     }
 }
