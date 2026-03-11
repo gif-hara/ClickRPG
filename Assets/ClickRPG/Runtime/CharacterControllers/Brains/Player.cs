d31c68a6a009116d44287a849bc0b31fe5713ea8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-12 00:02:14 +0900

    Playerクラスにマズルのスキルタイプと要素を追加し、発射処理を更新しました。

--- Assets/ClickRPG/Runtime/CharacterControllers/Brains/Player.cs
+++ Assets/ClickRPG/Runtime/CharacterControllers/Brains/Player.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using ClickRPG.ProjectileControllers;
+using HK;
 using R3;
 using R3.Triggers;
 using UnityEngine;
@@ -17,7 +19,10 @@ namespace ClickRPG.CharacterControllers.Brains
         private Transform muzzleObject = null!;
 
         [SerializeField]
-        private Muzzle muzzle = null!;
+        private CharacterSkillType[] muzzleSkillTypes = null!;
+
+        [SerializeField]
+        private MuzzleElement.DictionaryList muzzleElements = null!;
 
         public void Attach(Character character, CompositeDisposable scope)
         {
@@ -34,10 +39,50 @@ namespace ClickRPG.CharacterControllers.Brains
 
                     if (Mouse.current.leftButton.isPressed)
                     {
-                        @this.muzzle.Fire(character);
+                        foreach (var muzzleSkillType in @this.muzzleSkillTypes)
+                        {
+                            if (character.SkillPowers.TryGetValue(muzzleSkillType, out var power) && power > 0)
+                            {
+                                var muzzleElement = @this.muzzleElements.Get(muzzleSkillType);
+                                if (muzzleElement != null)
+                                {
+                                    var levelIndex = Mathf.Min(power - 1, muzzleElement.Levels.Length - 1);
+                                    var muzzleLevelElement = muzzleElement.Levels[levelIndex];
+                                    foreach (var muzzle in muzzleLevelElement.Muzzles)
+                                    {
+                                        muzzle.Fire(character);
+                                    }
+                                }
+                            }
+                        }
                     }
                 })
                 .AddTo(scope);
+        }
+
+        [Serializable]
+        public sealed class MuzzleElement
+        {
+            [field: SerializeField]
+            public CharacterSkillType Type { get; private set; } = default!;
+
+            [field: SerializeField]
+            public MuzzleLevelElement[] Levels { get; private set; } = null!;
+
+            [Serializable]
+            public sealed class DictionaryList : DictionaryList<CharacterSkillType, MuzzleElement>
+            {
+                public DictionaryList() : base(element => element.Type)
+                {
+                }
+            }
+        }
+
+        [Serializable]
+        public sealed class MuzzleLevelElement
+        {
+            [field: SerializeField]
+            public List<Muzzle> Muzzles { get; private set; } = new();
         }
     }
 }
