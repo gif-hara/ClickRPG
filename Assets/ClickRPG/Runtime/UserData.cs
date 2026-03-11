a0492ee22033cf6b68a0e77d2d0cb05d0936b188
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-11 23:31:18 +0900

    UserDataクラスにスキルリストのプロパティと設定メソッドを追加しました。

--- Assets/ClickRPG/Runtime/UserData.cs
+++ Assets/ClickRPG/Runtime/UserData.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using ClickRPG.CharacterControllers;
+using UnityEngine;
 
 namespace ClickRPG
 {
     [Serializable]
     public class UserData
     {
+        [field: SerializeField]
+        public List<Skill> Skills { get; private set; } = new();
+
+        public void SetSkills(IEnumerable<Skill> skills)
+        {
+            Skills.Clear();
+            Skills.AddRange(skills);
+        }
     }
 }
