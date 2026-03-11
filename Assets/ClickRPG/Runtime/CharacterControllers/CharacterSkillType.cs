448cb2bebd13be3feb819b35f1ea51128bce0c58
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-11 22:57:10 +0900

    CharacterSkillType列挙型を新規作成し、キャラクターのスキルタイプを定義しました。

---
+++ Assets/ClickRPG/Runtime/CharacterControllers/CharacterSkillType.cs
@@ -0,0 +1,24 @@
+namespace ClickRPG.CharacterControllers
+{
+    public enum CharacterSkillType
+    {
+        NormalBullet,
+        Shotgun,
+        MachineGun,
+        Sniper,
+        Laser,
+        Horming,
+        HitPointUp,
+        StrengthUp,
+        CooldownLevelUp,
+        CriticalRateUp,
+        CriticalDamageUp,
+        CooldownWhenCritical,
+        StrengthUpWhenDefeatEnemy,
+        CriticalRateUpWhenDefeatEnemy,
+        PenetrateUp,
+        BulletSpeedUp,
+        ExperienceUp,
+        RecoveryWhenDefeatEnemy,
+    }
+}
