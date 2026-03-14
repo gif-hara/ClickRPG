207edcffd5a83808bf7b929308d9231dbd0e61ce
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-14 16:19:39 +0900

    Effectクラスにフィードバック要素を追加し、オブジェクトプール機能を改善しました。

--- Assets/ClickRPG/Runtime/EffectControllers/Effect.cs
+++ Assets/ClickRPG/Runtime/EffectControllers/Effect.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
 using HKFeedback;
+using HKFeedback.Extensions;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -11,20 +15,22 @@ namespace ClickRPG.EffectControllers
         IProvider<Transform>,
         IProvider<IDisposable>
     {
+        [SerializeReference, SubclassSelector]
+        private IFeedback<Effect>[] onEnableFeedbacks = null!;
+
         Transform IProvider<Transform>.Provide() => transform;
 
         IDisposable IProvider<IDisposable>.Provide() => this;
 
         ObjectPool<Effect> pool;
 
+        private CancellationTokenSource lifetimeScope;
+
         public Effect Spawn()
         {
             pool ??= new ObjectPool<Effect>(
                 createFunc: () => Instantiate(this),
-                actionOnRelease: effect =>
-                {
-                    effect.gameObject.SetActive(false);
-                },
+                actionOnRelease: effect => effect.gameObject.SetActive(false),
                 actionOnGet: effect => effect.gameObject.SetActive(true)
                 );
             var instance = pool.Get();
@@ -42,6 +48,17 @@ namespace ClickRPG.EffectControllers
             {
                 Destroy(gameObject);
             }
+        }
+
+        private void OnEnable()
+        {
+            lifetimeScope = new CancellationTokenSource();
+            onEnableFeedbacks.PlayAsync(this, lifetimeScope.Token).Forget();
+        }
+
+        private void OnDisable()
+        {
+            lifetimeScope.Cancel();
         }
     }
 }
